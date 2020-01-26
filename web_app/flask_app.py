7e8c974c6469d1dbc9f02879dc7e6379232fc26f
Anthony <anthony.galtier@gmail.com>
2020-01-26 22:00:55 +0100

    web app pretty much functional

--- web_app/flask_app.py
+++ web_app/flask_app.py
@@ -1,15 +1,16 @@
 from flask import Flask, request, render_template
 from model_interface import writeSomeBars
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     if request.method == 'GET':
         return render_template('index.html', value='hi')
     if request.method == 'POST':
+        artist = request.form['artist']
         first_bar = request.form['first_bar']
-    next_bars = writeSomeBars(first_bar)
+    next_bars = writeSomeBars(artist, first_bar)
     return render_template('results.html', next_bars = next_bars)
 
 if __name__ == '__main__':
--- web_app/model_interface.py
+++ web_app/model_interface.py
@@ -1,8 +1,9 @@
 from lstm.lstm_model import *
 from lstm.lstm_predict import *
 
-def writeSomeBars(first_bars):
-    with open('./pytorch_models/model_booba_200_epoch.net', 'rb') as f:
+def writeSomeBars(artist, first_bars):
+    model_path = './pytorch_models/model_' + artist + '_200_epoch.net'
+    with open(model_path, 'rb') as f:
         checkpoint = torch.load(f)
 
     loaded = CharRNN(checkpoint['tokens'], n_hidden=checkpoint['n_hidden'], n_layers=checkpoint['n_layers'])
